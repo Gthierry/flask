9b937a82073ffd2f5fe7c2f91dcd333a7fd158c7
Gthierry <thierrygillot@live.com>
2025-08-26 12:19:54 +0200

    products & users changes

--- app/models/user.py
+++ app/models/user.py
@@ -1,2 +1,11 @@
-class Users:
-    pass
+from sqlalchemy import Column, Integer, String
+from app import db
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = Column("user_id", Integer, primary_key=True, autoincrement=True)
+    name = Column("user_name", String(50))
+    first_name = Column("user_firstname", String(50))
+    email = Column("user_email", String(60), nullable=False, unique=True)
